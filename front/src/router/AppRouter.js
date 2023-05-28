608391a87f04a133f22b96c11c4f7d6bf176e0f0
DaMarin94 <damian.marin@smartway.com.ar>
2023-05-28 20:09:55 -0300

    Header en router para todas las paginas

--- front/src/router/AppRouter.js
+++ front/src/router/AppRouter.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route,  BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from '../views/home';
+import Header from '../components/Header';
  
 export const AppRouter = () => { 
 
   return (
     <Router>
+      <Header/>
       <Routes>
         <Route  path='/' element={<Home />}/>
       </Routes>
--- front/src/views/home.js
+++ front/src/views/home.js
@@ -5,33 +5,6 @@ function Home() {
 
   return (
     <div className="App">
-      <nav class="navbar fixed-top navbar-expand-lg px-5 py-3">
-        <div class="container-fluid row">
-          <div class="col-6 text-start">
-            <a class="navbar-brand fw-bold fs-2 text-light" href="#">TUTICKET</a>
-          </div>
-
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-              aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"><i class="bi bi-list fs-1"></i></span>
-          </button>
-
-          <div class="col-6 collapse navbar-collapse my-2 my-lg-0 justify-content-end" id="navbarNav">
-           <ul class="navbar-nav text-center mt-md-3 mt-lg-0 flex-md-row flex-md-nowrap justify-content-center">
-              <li class="nav-item me-md-3 me-lg-0">
-                <a class="nav-link fs-5 text-light underline" href="#">Eventos</a>
-              </li>
-              <li class="nav-item me-md-3 mx-lg-3 m-auto">
-                <a class="nav-link fs-5 text-light underline" href="#">Ingresar</a>
-              </li>
-              <li class="nav-item m-auto">
-                <a class="nav-link fs-5 text-light underline" href="#">Registrarse</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
       <div class="pb-5">
         <div>
           <img class="w-100" height="550px" src="https://turismo.buenosaires.gob.ar/sites/turismo/files/teatro_colon_ensayo_san_nicolas1500x610.jpg"></img>
