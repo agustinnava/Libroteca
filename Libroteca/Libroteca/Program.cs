4e0c48d2d7698d4d38d2881047891a1f42b83e7f
AramiAquino <jaquinoromero@alumno.unlam.edu.ar>
2023-06-03 15:20:12 -0300

    Se agrego la dependencia de context en program.cs

--- Libroteca/Libroteca/Program.cs
+++ Libroteca/Libroteca/Program.cs
@@ -1,3 +1,5 @@
+using Libroteca.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<LibrotecaContext, LibrotecaContext>();
 
 var app = builder.Build();
 
